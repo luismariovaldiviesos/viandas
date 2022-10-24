0e531ad930940ff0b8b1bc33c8a56d523e4fde0d
luismariovaldiviesos <luismario.valdiviesos@gmail.com>
2022-10-24 15:09:50 -0500

    total iva 12

--- resources/views/livewire/sales/component.blade.php
+++ resources/views/livewire/sales/component.blade.php
@@ -238,7 +238,8 @@
                 </div>
                 <div class="mt-3">
                     <h1 class="text-2x1 font-bold">sub total 12%</h1>
-                    <h4 class="text-2x1">${{number_format($this->totIva,2)}}</h4>
+                    <h4 class="text-2x1"> ${{number_format($this->totIva * $item->qty,2)}}</h4>
+
                 </div>
                 <div class="mt-3">
                     <h1 class="text-2x1 font-bold">sub tot no objeto iva</h1>
